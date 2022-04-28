98db30854ee864894da133a603c6dc9e5c200d51
Alessioventuri <ale.venturi95@yahoo.it>
2022-04-28 21:20:37 +0200

    Update writer.hpp

--- writer.hpp
+++ writer.hpp
@@ -12,10 +12,9 @@ class writer {
 public:
     writer() = default;
     // Destructor of base class must always be virtual 
-    virtual ~writer() = default;  
-    //virtual void writeFile(string,NetworkLayout,Interlock,map<int,string>,map<int,string>,int) = 0;
-    virtual void writeFile(string, ParserXML *,int) = 0;
+    virtual ~writer() = default; 
+    virtual void writeFile(string,ParserXML *,int) = 0;
     static writer* write(fileType type);
-    friend class ParserXML;
 };
+
 
